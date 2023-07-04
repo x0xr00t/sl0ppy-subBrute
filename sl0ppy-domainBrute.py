898de39193709d95dda9052411af07b5d5a61f61
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2023-07-04 18:40:47 +0200

    Update sl0ppy-domainBrute.py

--- sl0ppy-domainBrute.py
+++ sl0ppy-domainBrute.py
@@ -1,10 +1,10 @@
-#!/env/python
+#!/usr/bin/env python
 # Team   : Sl0ppyr00t
 # AKA    : x0xr00t
 # Author : p.hoogeveen
 # Tool   : Sl0ppy-DomainBrute
 
-
+import sys
 import dns.resolver
 import string
 import itertools
@@ -13,7 +13,7 @@ from colorama import Fore, Style, init
 import random
 import threading
 import psutil
-
+from tqdm import tqdm
 
 def print_banner():
     init(autoreset=True)
@@ -24,7 +24,7 @@ def print_banner():
  /$$_____/| $$| $$ $$ $$ /$$__  $$ /$$__  $$| $$  | $$ /$$$$$$| $$$$$$$  /$$__  $$| $$  | $$|_  $$_/   /$$__  $$
 |  $$$$$$ | $$| $$\ $$$$| $$  \ $$| $$  \ $$| $$  | $$|______/| $$__  $$| $$  \__/| $$  | $$  | $$    | $$$$$$$$
  \____  $$| $$| $$ \ $$$| $$  | $$| $$  | $$| $$  | $$        | $$  \ $$| $$      | $$  | $$  | $$ /$$| $$_____/
- /$$$$$$$/| $$|  $$$$$$/| $$$$$$$/| $$$$$$$/|  $$$$$$$        | $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$
+ /$$$$$$$/| $$|  $$$$$$/| $$$$$$$/|  $$$$$$$|  $$$$$$$        | $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$
 |_______/ |__/ \______/ | $$____/ | $$____/  \____  $$        |_______/ |__/       \______/    \___/   \_______/
                         | $$      | $$       /$$  | $$                                                          
                         | $$      | $$      |  $$$$$$/                                                          
@@ -38,18 +38,17 @@ def print_banner():
  /$$$$$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$| $$            | $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$| $$      
 |_______/  \______/ |_______/  \_______/|__/|__/            |_______/ |__/       \______/    \___/   \_______/|__/    
     """
-    banner_colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+
+    color_order = [Fore.RED, Fore.YELLOW]
     colored_banner = ""
-    color_index = 0
+    line_num = 0
+
     for line in banner.splitlines():
-        colored_line = ""
-        for char in line:
-            if char.isalpha():
-                colored_line += banner_colors[color_index % len(banner_colors)] + char
-                color_index += 1
-            else:
-                colored_line += char
+        color = color_order[line_num % len(color_order)]
+        colored_line = f"{color}{line}"
         colored_banner += colored_line + "\n"
+        line_num += 1
+
     print(colored_banner)
     print(Style.RESET_ALL)
 
@@ -59,39 +58,37 @@ def brute_force_domains(domain):
     found_domains = []
 
     total_combinations = 0
-    for length in range(1, 101):  # Set the desired length of the subdomains (1 to 100 characters)
+    for length in range(1, 60):
         total_combinations += len(characters) ** length
 
-    current_combination = 0
-    for length in range(1, 101):
-        for combination in itertools.product(characters, repeat=length):
-            current_combination += 1
-            subdomain = ''.join(combination)
-            target = subdomain + '.' + domain
-            try:
-                answers = 'A' * 100
-                for answer in answers:
+    with tqdm(total=total_combinations, unit='combination', ncols=80, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]') as pbar:
+        for length in range(1, 60):
+            for combination in itertools.product(characters, repeat=length):
+                subdomain = ''.join(combination)
+                target = subdomain + '.' + domain
+                try:
+                    answers = 'A' * 100
+                    for answer in answers:
+                        pass
+                except dns.resolver.NXDOMAIN:
                     pass
-            except dns.resolver.NXDOMAIN:
-                pass
-            except dns.resolver.NoAnswer:
-                pass
-            except dns.resolver.NoNameservers:
-                pass
-            except dns.exception.Timeout:
-                pass
-            except dns.resolver.NoNameservers:
-                pass
-            else:
-                found_domains.append(target)
+                except dns.resolver.NoAnswer:
+                    pass
+                except dns.resolver.NoNameservers:
+                    pass
+                except dns.exception.Timeout:
+                    pass
+                except dns.resolver.NoNameservers:
+                    pass
+                else:
+                    found_domains.append(target)
 
-            # Check CPU usage and limit to 80%
-            if psutil.cpu_percent() > 80:
-                threading.Event().wait(0.1)  # Sleep for 100ms to reduce CPU usage
+                # Check CPU usage and limit to 80%
+                if psutil.cpu_percent() > 80:
+                    threading.Event().wait(0.1)  # Sleep for 100ms to reduce CPU usage
 
-            # Print progress percentage
-            progress = (current_combination / total_combinations) * 100
-            print(f"\rProgress: {progress:.2f}%", end='')
+                pbar.set_description(f'Testing: {subdomain}')  # Update the progress bar description
+                pbar.update(1)  # Increment the progress bar
 
     return found_domains
 
